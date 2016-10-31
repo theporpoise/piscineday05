b4a107b742f068c965365e051636563f11f48c01
Matthew Gould <matt@everettgould.com>
2016-10-31 16:44:04 -0700

    updating atoi to handle leading whitespace and commit to handle largest negative integer for putnbr

--- ex01/ft_putnbr.c
+++ ex01/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: mgould <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/31 08:55:48 by mgould            #+#    #+#             */
-/*   Updated: 2016/10/31 09:48:21 by mgould           ###   ########.fr       */
+/*   Updated: 2016/10/31 15:33:35 by mgould           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,17 @@ void	ft_putchar(char c);
 
 void	ft_putnbr(int nb)
 {
-	if (nb < 0)
+	long	nb_long;
+
+	nb_long = nb;
+	if (nb_long < 0)
 	{
 		ft_putchar('-');
-		nb *= -1;
+		nb_long *= -1;
 	}
-	if (nb > 9)
+	if (nb_long > 9)
 	{
-		ft_putnbr((nb / 10));
+		ft_putnbr((nb_long / 10));
 	}
-	ft_putchar((nb % 10) + '0');
+	ft_putchar((nb_long % 10) + '0');
 }
--- ex02/ft_atoi.c
+++ ex02/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: mgould <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/31 10:14:13 by mgould            #+#    #+#             */
-/*   Updated: 2016/10/31 12:25:26 by mgould           ###   ########.fr       */
+/*   Updated: 2016/10/31 16:42:13 by mgould           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,24 @@ int	ft_atoi(char *str)
 {
 	int sign;
 	int number;
-	int count;
 
 	number = 0;
 	sign = 1;
-	count = 0;
+	while	(*str == ' ' || *str == '\t' || *str == '\n' ||
+			*str == '\v' || *str == '\f' || *str == '\r')
+		str++;
 	if (*str == '-')
 	{
 		sign = sign * -1;
-		count++;
+		str++;
 	}
 	else if (*str == '+')
-	{
-		count++;
-	}
-	while ((str[count] > 47) && (str[count] < 58))
+		str++;
+	while ((*str > 47) && (*str < 58))
 	{
 		number *= 10;
-		number += str[count] - '0';
-		count++;
+		number += *str - '0';
+		str++;
 	}
 	return (number * sign);
 }
